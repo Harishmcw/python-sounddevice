5484e4bfc21e8c96b82946d4604024b06a38a6f6
Harish RS <harish.rajaselvan@multicorewareinc.com>
2025-11-10 15:36:39 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ if libname and os.path.isdir('_sounddevice_data/portaudio-binaries'):
     packages = ['_sounddevice_data']
     package_data = {'_sounddevice_data': ['portaudio-binaries/' + libname,
                                           'portaudio-binaries/README.md']}
-    if system == 'Windows'and 'SD_ENABLE_ASIO' in os.environ:
+    if system == 'Windows'
         package_data['_sounddevice_data'].append(
             'portaudio-binaries/' + libname_asio)
     zip_safe = False
